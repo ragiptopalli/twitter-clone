e25780cebda8a6cc2f192cd32645a9f3964afc73
ragiptopalli <ragiptopalli@gmail.com>
2022-09-15 21:55:16 +0200

    some small changes inside NewsArticleWidget

--- components/widgets/newsArticlesWidget/NewsArticlesWidget.js
+++ components/widgets/newsArticlesWidget/NewsArticlesWidget.js
@@ -2,16 +2,13 @@
 const News = ({ article }) => {
   return (
     <a href={article.url} target='_blank' rel='noreferrer'>
-      <div className='flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200'>
-        <div className=' space-y-0.5'>
+      <div className='px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200 border-b border-gray-200 '>
+        <div className='mt-3'>
           <h6 className='text-sm font-bold'>{article.title}</h6>
-          <p className='text-xs font-medium text-gray-500'>
-            {article.source.name}
-          </p>
         </div>
         <img
-          className='rounded-xl'
-          width='70'
+          className='rounded-xl mt-2'
+          width='240'
           height='70'
           src={article.urlToImage}
           alt={article.title}
