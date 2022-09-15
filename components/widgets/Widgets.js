d896934f156920ba3244bf42aaab324f15b8fc31
ragiptopalli <ragiptopalli@gmail.com>
2022-09-15 17:19:37 +0200

    created News Component and added news feed inside the Widged Component

--- components/widgets/Widgets.js
+++ components/widgets/Widgets.js
@@ -1,6 +1,11 @@
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
-const Widgets = () => {
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import News from '../news/News';
+
+const Widgets = ({ newsResults }) => {
+  const [articleNum, setArticleNum] = useState(3);
+
   return (
     <div className='xl:w-[600px] hidden lg:inline ml-8 space-y-5'>
       <div className='w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50'>
@@ -12,6 +17,19 @@ const Widgets = () => {
             placeholder='Search Twitter'
           />
         </div>
+      </div>
+
+      <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]'>
+        <h4 className='font-bold text-xl px-4'>What&apos;s happening</h4>
+        {newsResults.slice(0, articleNum).map((article) => (
+          <News key={article.url} article={article} />
+        ))}
+        <button
+          onClick={() => setArticleNum(articleNum + 3)}
+          className='text-blue-300 pl-4 pb-3 hover:text-blue-400'
+        >
+          Show more
+        </button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from '../components/feed/Feed';
 import Sidebar from '../components/sidebar/Sidebar';
 import Widgets from '../components/widgets/Widgets';
 
-export default function Home() {
+export default function Home({ newsResults }) {
   return (
     <div>
       <Head>
@@ -12,12 +12,28 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className='flex min-h-screen max-w-7xl mx-auto'>
-        <Sidebar />
+        <div>
+          <Sidebar />
+        </div>
 
         <Feed />
 
-        <Widgets />
+        <Widgets newsResults={newsResults.articles} />
       </main>
     </div>
   );
+}
+
+//https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json
+
+export async function getServerSideProps() {
+  const newsResults = await fetch(
+    'https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json'
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      newsResults,
+    },
+  };
 }
